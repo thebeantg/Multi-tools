3d0a8676bbdb93f46d10f3900d698592ca43b026
JEOL HERE! <102511853+Jeolpaul@users.noreply.github.com>
2022-08-31 18:40:49 +0530

    Update and rename 🚶 to pastebin.py

---
+++ plugins/utils/pastebin.py
@@ -0,0 +1,11 @@
+
+from plugins.utils.http import post
+
+BASE = "https://batbin.me/"
+
+
+async def paste(content: str):
+    resp = await post(f"{BASE}api/v2/paste", data=content)
+    if not resp["success"]:
+        return
+    return BASE + resp["message"]
