96b6141a7bf82d6f8ef89ad4751295763d9f7222
Kim So-hyun <110754631+Mr-BUG-TG@users.noreply.github.com>
2022-08-31 19:28:32 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,24 +1,26 @@
-from variables import DB_URL
-import pymongo 
-import os
+from variables import DB_URL, DB_NAME
+import motor.motor_asyncio, datetime
 
-DB_NAME = os.environ.get("DB_NAME","Cloud19")
-mongo = pymongo.MongoClient(DB_URL)
-db = mongo[DB_NAME]
-dbcol = db["user"]
+class Database:
+    
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
 
+    def new_user(self, id):
+        return dict(
+            id=id,            
+            join_date=datetime.date.today().isoformat(),            
+        )
 
-def insert(chat_id):
-    user_id = int(chat_id)
-    user_det = {"_id": user_id,"file_id": None, "caption": None}
-    try:
-      dbcol.insert_one(user_det)
-    except:
-      pass
+    async def add_user(self, id):
+        user = self.new_user(id)
+        await self.col.insert_one(user)
 
-def getid():
-    values = []
-    for key  in dbcol.find():
-         id = key["_id"]
-         values.append((id)) 
-    return values
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return True if user else False
+
+ 
+db = Database(DB_URL, DB_NAME)
