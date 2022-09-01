33e3dbb7e33440e6bc644fc832e12764b0e3f265
JEOL HERE! <102511853+Jeolpaul@users.noreply.github.com>
2022-09-01 17:11:41 +0530

    Update carbon.py

--- plugins/carbon.py
+++ plugins/carbon.py
@@ -1,8 +1,33 @@
-from pyrogram import filters, Client
+from pyrogram import filters, Client as bot
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from helper.errors import capture_err
 from plugins.utils.functions import make_carbon
+from asyncio import gather
+from datetime import datetime, timedelta
+from io import BytesIO
+from math import atan2, cos, radians, sin, sqrt
+from os import execvp
+from random import randint
+from re import findall
+from re import sub as re_sub
+from sys import executable
+from aiohttp import ClientSession
+
+import aiofiles
+import speedtest
+from PIL import Image
+from pyrogram.types import Message
+
+aiohttpsession = ClientSession()
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiohttpsession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
+
 
 C = "**MADE WITH ❤️ BY >JEOL**"
 F = InlineKeyboardMarkup(
@@ -12,23 +37,20 @@ F = InlineKeyboardMarkup(
 )
 
 
-@Client.on_message(
-    filters.command("carbon")
-    & ~filters.edited
-)
-@capture_err
+@bot.on_message(filters.command("carbon"))
 async def carbon_func(_, message):
     if not message.reply_to_message:
         return await message.reply_text(
-            "Reply to a text message to make carbon."
+            "ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ."
         )
     if not message.reply_to_message.text:
         return await message.reply_text(
-            "Reply to a text message to make carbon."
+            "ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ."
         )
-    m = await message.reply_text("Preparing Carbon")
+    user_id = message.from_user.id
+    m = await message.reply_text("ᴘʀᴏᴄᴇssɪɴɢ...")
     carbon = await make_carbon(message.reply_to_message.text)
-    await m.edit("Uploading")
+    await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
     await message.reply_photo(
         photo=carbon,
         caption=C,
