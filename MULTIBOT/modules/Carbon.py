3ff76e45288b4305955ceeb33df666e6a7b63764
JEOL HERE! <102511853+Jeolpaul@users.noreply.github.com>
2022-09-03 18:45:08 +0530

    Create Carbon.py

---
+++ MULTIBOT/modules/Carbon.py
@@ -0,0 +1,66 @@
+from pyrogram import filters, Client as bot
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from multibot import Client
+from helper.errors import capture_err
+from plugins.utils.functions import make_carbon
+from asyncio import gather
+from datetime import datetime, timedelta
+from io import BytesIO
+from math import atan2, cos, radians, sin, sqrt
+from os import execvp
+from random import randint
+from re import findall
+from re import sub as re_sub
+from sys import executable
+from aiohttp import ClientSession
+
+import aiofiles
+import speedtest
+from PIL import Image
+from pyrogram.types import Message
+
+__MODULE__ = "Carbon"
+__HELP__ = """
+First send me any text and reply
+It by /carbon
+"""
+
+
+aiohttpsession = ClientSession()
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiohttpsession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
+
+
+C = "**MADE WITH ❤️ BY >JEOL**"
+F = InlineKeyboardMarkup(
+[[
+     InlineKeyboardButton("JOIN CHANNEL", url="https://t.me/beta_boTZ")
+]]
+)
+
+
+@Client.on_message(filters.command("carbon"))
+async def carbon_func(_, message):
+    if not message.reply_to_message:
+        return await message.reply_text(
+            "REPLY TO A TEXT MESSAGE TO MAKE CARBON."
+        )
+    if not message.reply_to_message.text:
+        return await message.reply_text(
+            "REPLY TO A TEXT MESSAGE TO MAKE CARBON."
+        )
+    user_id = message.from_user.id
+    m = await message.reply_text("Processing...")
+    carbon = await make_carbon(message.reply_to_message.text)
+    await m.edit("Uploading..")
+    await message.reply_photo(
+        photo=carbon,
+        caption=C,
+        reply_markup=F)
+    await m.delete()
+    carbon.close()
