0542f9b2003185e84574649b05954b76bea1b225
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-09-03 12:31:35 +0530

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,20 +1,8 @@
 import os
 import re
-import logging
-import logging.config
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from variables import FORCE_SUB
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-
-API_ID = int(os.environ.get("API_ID", ""))
-
-API_HASH = os.environ.get("API_HASH", "")
-
-logging.config.fileConfig("logging.conf")
-logging.getLogger().setLevel(logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
+from variables import *
 
 class App(Client):
 
@@ -33,23 +21,10 @@ class App(Client):
        self.id = me.id
        self.name = me.first_name
        self.mention = me.mention
-       self.username = '@' + me.username        
-       self.force_channel = FORCE_SUB
-       if FORCE_SUB:
-         try:
-            link = await self.export_chat_invite_link(FORCE_SUB)
-            self.invitelink = link
-         except Exception as e:
-            logging.warning(e) 
-            logging.warning("Make Sure Bot admin in force sub channel") 
-            self.force_channel = None
-       logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on @{me.username}.")          
+       self.username = me.username
 
-    async def stop(self, *args):
-       await super().stop()
-       logging.info("🚀Restarting..")
-
-
+       
+print('bot started......⚡️')  
 bot = App()
 bot.run()
 
