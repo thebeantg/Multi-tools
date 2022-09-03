42b5a28fafb530d5652741a37360765e19c1023e
JEOL HERE! <102511853+Jeolpaul@users.noreply.github.com>
2022-09-03 23:33:13 +0530

    Update misc.py

--- plugins/utils/misc.py
+++ plugins/utils/misc.py
@@ -70,12 +70,12 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
                 ] + [
                     (
                         EqInlineKeyboardButton(
-                            "❮",
+                            "◁",
                             callback_data="{}_prev({})".format(prefix,
                                                                modulo_page),
                         ),
                         EqInlineKeyboardButton(
-                            "Back",
+                            "◁ GO BACK",
                             callback_data="{}_home({})".format(prefix,
                                                                modulo_page),
                         ),
