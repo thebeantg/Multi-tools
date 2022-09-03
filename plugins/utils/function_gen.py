e077c0cb42f4a2977fc37a1bcec302780d14079b
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-09-03 18:45:36 +0530

    Create function_gen.py

---
+++ plugins/utils/function_gen.py
@@ -0,0 +1,242 @@
+import asyncio
+import re
+import ast
+from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty          
+import pyrogram
+
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
+from image.edit_1 import (  # pylint:disable=import-error
+    bright,
+    mix,
+    black_white,
+    g_blur,
+    normal_blur,
+    box_blur,
+)
+from image.edit_2 import (  # pylint:disable=import-error
+    circle_with_bg,
+    circle_without_bg,
+    sticker,
+    edge_curved,
+    contrast,
+    sepia_mode,
+    pencil,
+    cartoon,
+)
+from image.edit_3 import (  # pylint:disable=import-error
+    green_border,
+    blue_border,
+    black_border,
+    red_border,
+)
+from image.edit_4 import (  # pylint:disable=import-error
+    rotate_90,
+    rotate_180,
+    rotate_270,
+    inverted,
+    round_sticker,
+    removebg_white,
+    removebg_plain,
+    removebg_sticker,
+)
+from image.edit_5 import (  # pylint:disable=import-error
+    normalglitch_1,
+    normalglitch_2,
+    normalglitch_3,
+    normalglitch_4,
+    normalglitch_5,
+    scanlineglitch_1,
+    scanlineglitch_2,
+    scanlineglitch_3,
+    scanlineglitch_4,
+    scanlineglitch_5,
+)
+
+
+
+@Client.on_callback_query()
+async def cb_handler(client: Client, query: CallbackQuery):
+    if query.data == "close_data":
+        await query.message.delete()
+    elif query.data == "removebg":
+        await query.message.edit_text(
+            "**Select required mode**ㅤㅤㅤㅤ",
+            reply_markup=InlineKeyboardMarkup(
+                [[
+                InlineKeyboardButton(text="𝖶𝗂𝗍𝗁 𝖶𝗁𝗂𝗍𝖾 𝖡𝖦", callback_data="rmbgwhite"),
+                InlineKeyboardButton(text="𝖶𝗂𝗍𝗁𝗈𝗎𝗍 𝖡𝖦", callback_data="rmbgplain"),
+                ],[
+                InlineKeyboardButton(text="𝖲𝗍𝗂𝖼𝗄𝖾𝗋", callback_data="rmbgsticker"),
+                ],[
+                InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='photo')
+             ]]
+        ),)
+    elif query.data == "stick":
+        await query.message.edit(
+            "**Select a Type**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text="𝖭𝗈𝗋𝗆𝖺𝗅", callback_data="stkr"),
+                        InlineKeyboardButton(
+                            text="𝖤𝖽𝗀𝖾 𝖢𝗎𝗋𝗏𝖾𝖽", callback_data="cur_ved"
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            text="𝖢𝗂𝗋𝖼𝗅𝖾", callback_data="circle_sticker"
+                        )
+                    ],
+                    [
+                        InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='photo')
+                    ],
+                ]
+            ),
+        )
+    elif query.data == "rotate":
+        await query.message.edit_text(
+            "**Select the Degree**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text="180", callback_data="180"),
+                        InlineKeyboardButton(text="90", callback_data="90"),
+                    ],
+                    [InlineKeyboardButton(text="270", callback_data="270")],
+                    ],
+                    [
+                        InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='photo')
+                ]
+            ),
+        )
+    elif query.data == "glitch":
+        await query.message.edit_text(
+            "**Select required mode**ㅤㅤㅤㅤ",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="𝖭𝗈𝗋𝗆𝖺𝗅", callback_data="normalglitch"
+                        ),
+                        InlineKeyboardButton(
+                            text="𝖲𝖼𝖺𝗇 𝖫𝖺𝗂𝗇𝗌", callback_data="scanlineglitch"
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='photo')
+                    ]
+                ]
+            ),
+        )
+    elif query.data == "normalglitch":
+        await query.message.edit_text(
+            "**Select Glitch power level**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text="1", callback_data="normalglitch1"),
+                        InlineKeyboardButton(text="2", callback_data="normalglitch2"),
+                        InlineKeyboardButton(text="3", callback_data="normalglitch3"),
+                    ],
+                    [
+                        InlineKeyboardButton(text="4", callback_data="normalglitch4"),
+                        InlineKeyboardButton(text="5", callback_data="normalglitch5"),
+                    ],
+                    [
+                        InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='glitch')
+                    ],
+                ]
+            ),
+        )
+    elif query.data == "scanlineglitch":
+        await query.message.edit_text(
+            "**Select Glitch power level**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text="1", callback_data="scanlineglitch1"),
+                        InlineKeyboardButton(text="2", callback_data="scanlineglitch2"),
+                        InlineKeyboardButton(text="3", callback_data="scanlineglitch3"),
+                    ],
+                    [
+                        InlineKeyboardButton(text="4", callback_data="scanlineglitch4"),
+                        InlineKeyboardButton(text="5", callback_data="scanlineglitch5"),
+                    ],
+                    [
+                        InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='glitch')
+                    ],
+                ]
+            ),
+        )
+    elif query.data == "blur":
+        await query.message.edit(
+            "**Select a Type**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text="𝖡𝗈𝗑", callback_data="box"),
+                        InlineKeyboardButton(text="𝖭𝗈𝗋𝗆𝖺𝗅", callback_data="normal"),
+                    ],
+                    [InlineKeyboardButton(text="𝖦𝖺𝗎𝗌𝗌𝗂𝖺𝗇", callback_data="gas")],
+                    ],
+                    [
+                        InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='photo')
+                ]
+            ),
+        )
+    elif query.data == "circle":
+        await query.message.edit_text(
+            "**Select required mode**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text="𝖶𝗂𝗍𝗁 𝖡𝖦", callback_data="circlewithbg"),
+                        InlineKeyboardButton(text="𝖶𝗂𝗍𝗁𝗈𝗎𝗍 𝖡𝖦", callback_data="circlewithoutbg"),
+                    ],
+                    [
+                        InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='photo')
+                    ]
+                ]
+            ),
+        )
+    elif query.data == "border":
+        await query.message.edit(
+            "**Select Border**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text="𝖱𝖾𝖽", callback_data="red"),
+                        InlineKeyboardButton(text="𝖦𝗋𝖾𝖾𝗇", callback_data="green"),
+                    ],
+                    [
+                        InlineKeyboardButton(text="𝖡𝗅𝖺𝖼𝗄", callback_data="black"),
+                        InlineKeyboardButton(text="𝖡𝗅𝗎𝖾", callback_data="blue"),
+                    ],
+                    [
+                        InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='photo')   
+                    ],
+                ]
+            ),
+        )
+
+
+
+
+
+
+
+
+
+
+
+
+
