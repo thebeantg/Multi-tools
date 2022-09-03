6a71265d3f92073efb9a7bb8f53b7fe28c053425
JEOL HERE! <102511853+Jeolpaul@users.noreply.github.com>
2022-09-03 14:09:52 +0530

    Cheriya oru change😌

--- plugins/Broadcast.py
+++ plugins/Broadcast.py
@@ -16,7 +16,7 @@ logger.setLevel(logging.INFO)
 async def get_stats(bot :Client, message: Message):
     mr = await message.reply('**𝙰𝙲𝙲𝙴𝚂𝚂𝙸𝙽𝙶 𝙳𝙴𝚃𝙰𝙸𝙻𝚂.....**')
     total_users = await db.total_users_count()
-    await mr.edit( text=f"❤️‍🔥 TOTAL USER'S = `{total_users}`")
+    await mr.edit( text=f"🔍 TOTAL USER'S = `{total_users}`")
 
 @Client.on_message(filters.command("broadcast") & filters.user(ADMIN) & filters.reply)
 async def broadcast_handler(bot: Client, m: Message):
