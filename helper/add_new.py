dd2d5d5f5506d8f2cfe00340e4182e005a633993
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-09-03 14:20:29 +0530

    Update add_new.py

--- helper/add_new.py
+++ helper/add_new.py
@@ -10,7 +10,7 @@ async def add_user(bot: Client, msg: Message):
     if not await db.is_user_exist(msg.from_user.id):
         await db.add_user(msg.from_user.id)
         if LOG_CHANNEL is not None:            
-            await bot.send_flooded_message(LOG_CHANNEL, text=LOG_TEXT.format(id=msg.from_user.id, dc_id=msg.from_user.dc_id, first_name=msg.from_user.first_name, username=msg.from_user.username, bot=bot.mention))
+            await bot.send_message(LOG_CHANNEL, text=LOG_TEXT.format(id=msg.from_user.id, dc_id=msg.from_user.dc_id, first_name=msg.from_user.first_name, username=msg.from_user.username, bot=bot.mention))
 
                 
 
