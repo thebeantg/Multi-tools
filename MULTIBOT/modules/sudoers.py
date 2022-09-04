5bbb46359c9dee53f9c6a347b43a6a5c9d73505e
JEOL HERE! <102511853+Jeolpaul@users.noreply.github.com>
2022-09-04 13:38:21 +0530

    Update sudoers.py

--- MULTIBOT/modules/sudoers.py
+++ MULTIBOT/modules/sudoers.py
@@ -10,8 +10,7 @@ from pyrogram.errors import FloodWait
 
 from MULTIBOT import (
     BOT_ID,
-    GBAN_LOG_GROUP_ID,
-    SUDOERS,
+    ADMIN as SUDOERS,
     Client as app,
     bot_start_time,
 )
@@ -29,19 +28,7 @@ __MODULE__ = "Sudoers"
 __HELP__ = """
 /stats - To Check System Status.
 
-/gstats - To Check Bot's Global Stats.
-
-/gban - To Ban A User Globally.
-
-/clean_db - Clean database.
-
 /broadcast - To Broadcast A Message To All Groups.
-
-/update - To Update And Restart The Bot
-
-/eval - Execute Python Code
-
-/sh - Execute Shell Code
 """
 
 
@@ -68,87 +55,7 @@ DISK: {disk}%
 # Gban
 
 
-@app.on_message(filters.command("gban") & SUDOERS & ~filters.edited)
-@capture_err
-async def ban_globally(_, message):
-    user_id, reason = await extract_user_and_reason(message)
-    user = await app.get_users(user_id)
-    from_user = message.from_user
 
-    if not user_id:
-        return await message.reply_text("I can't find that user.")
-    if not reason:
-        return await message.reply("No reason provided.")
-
-    if user_id in [from_user.id, BOT_ID] or user_id in SUDOERS:
-        return await message.reply_text("I can't ban that user.")
-
-    served_chats = await get_served_chats()
-    m = await message.reply_text(
-        f"**Banning {user.mention} Globally!**"
-        + f" **This Action Should Take About {len(served_chats)} Seconds.**"
-    )
-    await add_gban_user(user_id)
-    number_of_chats = 0
-    for served_chat in served_chats:
-        try:
-            await app.ban_chat_member(served_chat["chat_id"], user.id)
-            number_of_chats += 1
-            await asyncio.sleep(1)
-        except FloodWait as e:
-            await asyncio.sleep(int(e.x))
-        except Exception:
-            pass
-    try:
-        await app.send_message(
-            user.id,
-            f"Hello, You have been globally banned by {from_user.mention},"
-            + " You can appeal for this ban by talking to him.",
-        )
-    except Exception:
-        pass
-    await m.edit(f"Banned {user.mention} Globally!")
-    ban_text = f"""
-__**New Global Ban**__
-**Origin:** {message.chat.title} [`{message.chat.id}`]
-**Admin:** {from_user.mention}
-**Banned User:** {user.mention}
-**Banned User ID:** `{user_id}`
-**Reason:** __{reason}__
-**Chats:** `{number_of_chats}`"""
-    try:
-        m2 = await app.send_message(
-            GBAN_LOG_GROUP_ID,
-            text=ban_text,
-            disable_web_page_preview=True,
-        )
-        await m.edit(
-            f"Banned {user.mention} Globally!\nAction Log: {m2.link}",
-            disable_web_page_preview=True,
-        )
-    except Exception:
-        await message.reply_text(
-            "User Gbanned, But This Gban Action Wasn't Logged, Add Me Bot In GBAN_LOG_GROUP"
-        )
-
-
-# Ungban
-
-
-@app.on_message(filters.command("ungban") & SUDOERS & ~filters.edited)
-@capture_err
-async def unban_globally(_, message):
-    user_id = await extract_user(message)
-    if not user_id:
-        return await message.reply_text("I can't find that user.")
-    user = await app.get_users(user_id)
-
-    is_gbanned = await is_gbanned_user(user.id)
-    if not is_gbanned:
-        await message.reply_text("I don't remember Gbanning him.")
-    else:
-        await remove_gban_user(user.id)
-        await message.reply_text(f"Lifted {user.mention}'s Global Ban.'")
 
 
 # Broadcast
