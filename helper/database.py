4fb6a3d8b01b5e0772f1676ae565caf20c952ab7
JEOL HERE! <102511853+Jeolpaul@users.noreply.github.com>
2022-09-04 15:42:05 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,37 +1,24 @@
-from variables import DB_URL, DB_NAME
-import motor.motor_asyncio, datetime
+from variables import DB_URL
+import pymongo 
+import os
 
-class Database:
-    
-    def __init__(self, uri, database_name):
-        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.db = self._client[database_name]
-        self.col = self.db.users
+DB_NAME = os.environ.get("DB_NAME","Cloud19")
+mongo = pymongo.MongoClient(DB_URL)
+db = mongo[DB_NAME]
+dbcol = db["user"]
 
-    def new_user(self, id):
-        return dict(
-            id=id,            
-            join_date=datetime.date.today().isoformat(),           
-        )
 
-    async def add_user(self, id):
-        user = self.new_user(id)
-        await self.col.insert_one(user)
+def insert(chat_id):
+    user_id = int(chat_id)
+    user_det = {"_id": user_id,"file_id": None, "caption": None}
+    try:
+      dbcol.insert_one(user_det)
+    except:
+      pass
 
-    async def is_user_exist(self, id):
-        user = await self.col.find_one({'id': int(id)})
-        return True if user else False
-
-    async def total_users_count(self):
-        count = await self.col.count_documents({})
-        return count
-  
-    async def get_all_users(self):
-        all_users = self.col.find({})
-        return all_users
-
-    async def delete_user(self, user_id):
-        await self.col.delete_many({'id': int(user_id)})
-
- 
-db = Database(DB_URL, DB_NAME)
+def getid():
+    values = []
+    for key  in dbcol.find():
+         id = key["_id"]
+         values.append((id)) 
+    return values
