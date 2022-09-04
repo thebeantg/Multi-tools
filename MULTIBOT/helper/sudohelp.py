94ed55e886166d13826b1aa444f703b342ebf5f0
JEOL HERE! <102511853+Jeolpaul@users.noreply.github.com>
2022-09-04 07:37:21 +0530

    Update sudohelp.py

--- MULTIBOT/helper/sudohelp.py
+++ MULTIBOT/helper/sudohelp.py
@@ -7,7 +7,7 @@ import datetime
 from variables import ADMIN
 from helper.database import db
 from pyrogram.types import Message
-from pyrogram import Client, filters
+from pyrogram import filters
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 
 logger = logging.getLogger(__name__)
