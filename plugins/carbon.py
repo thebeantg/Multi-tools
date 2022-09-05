beff6c98ea9e34ec284a88b519648da3920cf01b
JEOL HERE! <102511853+Jeolpaul@users.noreply.github.com>
2022-09-05 14:47:09 +0530

    Update carbon.py

--- plugins/carbon.py
+++ plugins/carbon.py
@@ -1,18 +1,9 @@
 from pyrogram import filters, Client as bot
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from asyncio import gather
-from datetime import datetime, timedelta
-from io import BytesIO
-from math import atan2, cos, radians, sin, sqrt
-from os import execvp
-from random import randint
-from re import findall
-from re import sub as re_sub
-from sys import executable
 from aiohttp import ClientSession
 
 import aiofiles
-import speedtest
 from PIL import Image
 from pyrogram.types import Message
 
