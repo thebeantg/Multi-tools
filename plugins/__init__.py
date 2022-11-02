e4b5101f9d329cd94e3fe3bb80b5135b1b34a4e3
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-11-02 20:09:42 +0530

    Update to v3.0.1

---
+++ plugins/__init__.py
@@ -0,0 +1,11 @@
+
+
+class txt(object):
+    INFO_TXT = """<i>
+<u>👁️‍🗨️YOUR DETAILS</u>
+○ ID : <code>{id}</code>
+○ DC : <code>{dc}</code>
+○ First Name : <code>{n}<code>
+○ UserName : @{u}
+○ link : <code>https://t.me/{u}</code>
+Thank You For Using Me❣️</i>"""
