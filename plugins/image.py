81cf7118f9667618dbe0dabf283efcf9a06ad8d2
Mrbeanbot <92094675+Mrbeanbot@users.noreply.github.com>
2022-11-09 10:55:25 +0530

    Update image.py

--- plugins/image.py
+++ plugins/image.py
@@ -12,34 +12,34 @@ async def photo(client: Client, message: Message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton(text="𝖡𝗋𝗂𝗀𝗍𝗁", callback_data="bright"),
-                        InlineKeyboardButton(text="𝖬𝗂𝗑𝖾𝖽", callback_data="mix"),
-                        InlineKeyboardButton(text="𝖡 & 𝖶", callback_data="b|w"),
+                        InlineKeyboardButton(text="Bʀɪɢʜᴛ", callback_data="bright"),
+                        InlineKeyboardButton(text="Mɪxᴇᴅ", callback_data="mix"),
+                        InlineKeyboardButton(text="Bʟᴀᴄᴋ & ᴡʜɪᴛᴇ", callback_data="b|w"),
                     ],
                     [
-                        InlineKeyboardButton(text="𝖢𝗂𝗋𝖼𝗅𝖾", callback_data="circle"),
-                        InlineKeyboardButton(text="𝖡𝗅𝗎𝗋", callback_data="blur"),
-                        InlineKeyboardButton(text="𝖡𝗈𝗋𝖽𝖾𝗋", callback_data="border"),
+                        InlineKeyboardButton(text="Cɪʀᴄʟᴇ", callback_data="circle"),
+                        InlineKeyboardButton(text="Bʟᴜʀ", callback_data="blur"),
+                        InlineKeyboardButton(text="Bᴏʀᴅᴇʀ", callback_data="border"),
                     ],
                     [
-                        InlineKeyboardButton(text="𝖲𝗍𝗂𝖼𝗄𝖾𝗋", callback_data="stick"),
-                        InlineKeyboardButton(text="𝖱𝗈𝗍𝖺𝗍𝖾", callback_data="rotate"),
-                        InlineKeyboardButton(text="𝖢𝗈𝗇𝗍𝗋𝖺𝗌𝗍", callback_data="contrast"),
+                        InlineKeyboardButton(text="Sᴛɪᴄᴋᴇʀ", callback_data="stick"),
+                        InlineKeyboardButton(text="Rᴏᴛᴀᴛᴇ", callback_data="rotate"),
+                        InlineKeyboardButton(text="Cᴏɴᴛʀᴀꜱᴛ", callback_data="contrast"),
                     ],
                     [
-                        InlineKeyboardButton(text="𝖲𝖾𝗉𝗂𝖺", callback_data="sepia"),
-                        InlineKeyboardButton(text="𝖯𝖾𝗇𝖼𝗂𝗅", callback_data="pencil"),
-                        InlineKeyboardButton(text="𝖢𝖺𝗋𝗍𝗈𝗈𝗇", callback_data="cartoon"),
+                        InlineKeyboardButton(text="Sᴇᴩɪᴀ", callback_data="sepia"),
+                        InlineKeyboardButton(text="Pᴇɴᴄɪʟ", callback_data="pencil"),
+                        InlineKeyboardButton(text="Cᴀʀᴛᴏᴏɴ", callback_data="cartoon"),
                     ],
                     [
-                        InlineKeyboardButton(text="𝖨𝗇𝗏𝖾𝗋𝗍", callback_data="inverted"),
-                        InlineKeyboardButton(text="𝖦𝗅𝗂𝗍𝖼𝗁", callback_data="glitch"),
+                        InlineKeyboardButton(text="Iɴᴠᴇʀᴛ", callback_data="inverted"),
+                        InlineKeyboardButton(text="Gʟɪᴛᴄʜ", callback_data="glitch"),
                         InlineKeyboardButton(
-                            text="𝖱𝖾𝗆𝗈𝗏𝖾 𝖡𝖦", callback_data="removebg"
+                            text="Rᴇᴍᴏᴠᴇ ʙɢ", callback_data="removebg"
                         ),
                     ],
                     [
-                        InlineKeyboardButton(text="𝖢𝗅𝗈𝗌𝖾", callback_data="close_data"),
+                        InlineKeyboardButton(text="↻ ᴄʟᴏꜱᴇ ↻", callback_data="close_data"),
                     ],
                 ]
             ),
