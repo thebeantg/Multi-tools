feac24d6f6c673dbcf48d6fc0a41d5f2b23034f0
Mrbeanbot <92094675+Mrbeanbot@users.noreply.github.com>
2022-11-09 08:27:01 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -83,15 +83,4 @@ async def media_info(bot, m):
                disable_notification=True
            )
 
-@Client.on_message(filters.command("repo"))
-async def repo(client, message):
-    users = await get("https://api.github.com/repos/jeolpaul/TG-MULTI-BOT/contributors")
-    list_of_users = ""
-    count = 1
-    for user in users:
-        list_of_users += (f"**{count}.** [{user['login']}]({user['html_url']})\n")       
-        count += 1
-    text = f"""[Github](https://github.com/Jeolpaul/TG-MULTI-BOT) | [Updates](t.me/beta_botz)\n```----------------\n| Contributors |\n----------------```\n{list_of_users}"""
-    await client.send_message(chat_id=message.chat.id, text=text, disable_web_page_preview=True)
 
-    
