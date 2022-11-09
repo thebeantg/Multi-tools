037944ad6be07870f5eb2142ef783380a0cb1aaf
Mrbeanbot <92094675+Mrbeanbot@users.noreply.github.com>
2022-11-09 11:44:43 +0530

    Update yt_dl.py

--- plugins/yt_dl.py
+++ plugins/yt_dl.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**BY›› [TG MULTI BOT](https://github.com/Jeolpaul/TG-MULTI-BOT)**'
+        rep = '**BY›› [MULTI TOOLS BOT](https://github.com/Mrbeanbot/multi-bot)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
