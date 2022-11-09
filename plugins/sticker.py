bcbd94046d034ed3053720cb7ce5fccd3421a8b8
Mrbeanbot <92094675+Mrbeanbot@users.noreply.github.com>
2022-11-09 11:10:02 +0530

    Update sticker.py

--- plugins/sticker.py
+++ plugins/sticker.py
@@ -158,7 +158,7 @@ async def kang(client, message: Message):
                 stickerset = await create_sticker_set(
                     client,
                     message.from_user.id,
-                    f"{message.from_user.first_name[:32]}'s | @BETA_BOTZ Pack",
+                    f"{message.from_user.first_name[:32]}'s | @HELL_BOTZ Pack",
                     packname,
                     [sticker],
                 )
